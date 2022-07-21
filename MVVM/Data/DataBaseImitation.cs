22f8114127cc02b846d6c0239ea6b6fbb20f2afa
Andriy Gth <perebeynos19856@gmail.com>
2022-07-21 11:26:38 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Data/DataBaseImitation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MVVM.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Data
+{
+    public class DataBaseImitation
+    {
+        public static List<Person> GetPeople() 
+        { 
+            
+            return new List<Person>()
+            {
+               new Person() { FirstName = "Bill", LastName = "Gates" },
+               new Person() { FirstName = "Paul", LastName = "Alen" },
+               new Person() { FirstName = "Leslie", LastName = "Lamport" }
+
+            };
+            
+        }
+    }
+}
